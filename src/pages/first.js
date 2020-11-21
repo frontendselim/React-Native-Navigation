91fc4a9531809b0ba01e91edbe336ec5cb525764
selim <sahhgaga@gmail.com>
2020-11-22 02:51:42 +0300

    first lesson done

--- src/pages/first.js
+++ src/pages/first.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import { SafeAreaView, View, Text, Button } from 'react-native';
+import React, {useState} from 'react';
+import { SafeAreaView, View, Text, Button, TextInput } from 'react-native';
 
 const number = 55;
 
 const First = (props) => {
+    const [userName, setUserName] = useState("");
     return(
         <SafeAreaView>
             <View>
                 <Text style={{fontSize:50}}> First</Text>
                 <Text style={{fontSize:50}}> {number}</Text>
+
+                <Text style={{fontWeight:'bold', margin:5}}>Girilen değer : {userName} </Text>
+
+                <View style={{backgroundColor:'#e0e0e0', padding:10, margin:10, borderRadius:5}}>
+                    <TextInput
+                    value={userName}
+                    onChangeText= { (text) => setUserName(text) }
+
+                    />
+                </View>
                 <Button
                     title="Go!"
-                    onPress={() => props.navigation.navigate('SecondPage',{ myNumber:number})}
+                    onPress={() => props.navigation.navigate('SecondPage',{ myNumber:number, name:userName})}
                 />
             </View>
         </SafeAreaView>
--- src/pages/second.js
+++ src/pages/second.js
@@ -4,11 +4,12 @@ import { SafeAreaView, View, Text, Button } from 'react-native';
 
 const Second = (props) => {
     const userNumber = props.route.params.myNumber
+    const userValue =props.route.params.name
     return(
         <SafeAreaView>
             <View>
                 <Text style={{fontSize:50}}> Second</Text>
-                <Text style={{fontSize:50}}> {userNumber}</Text>
+                <Text style={{fontSize:50}}> girilen: {userValue}</Text>
                 <Button title="Go back" onPress={() => props.navigation.goBack()} />
             </View>
         </SafeAreaView>
